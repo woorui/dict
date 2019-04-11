260be64127797822a817ae77b4b5e3daad37d8e4
wurui <rui1009479218@gmail.com>
2019-04-11 17:10:14 +0800

    separate information file operation

--- information.go
+++ information.go
@@ -12,42 +12,83 @@ import (
 
 const saveInformationFileName = ".simple_dict"
 
-// Information is applied from baidu open api
+// homedir is user's home directory, For example It is /home/$USER in linux environment
+var homeDir string
+
+func init() {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatalln(errors.New("Can not get homeDir" + err.Error()))
+	}
+	homeDir = usr.HomeDir
+}
+
+// Information was applied from baidu open api
 // apply link: http://api.fanyi.baidu.com/api/trans/product/index
 type Information struct {
 	Appid  string `json:"appid"`
 	Secret string `json:"secret"`
 }
 
-// homedir is user's home directory, For example It is /home/$USER in linux environment
-var homeDir = (func() string {
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatalln(errors.New("Can not get homeDir" + err.Error()))
+// readFromFile read SomeThing from SomeFile
+func readFromFile(filePath string) ([]byte, error) {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return nil, errors.New("filepath " + filePath + " does not exist")
 	}
-	return usr.HomeDir
-})()
+	b, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
 
+// getInformation get information that cantain appid and secret from ~/.simple-dict
 func getInformation() (*Information, error) {
 	var info Information
-	fpd := filepath.Join(homeDir, saveInformationFileName)
-	if _, err := os.Stat(fpd); os.IsNotExist(err) {
-		return &info, errors.New("file named " + saveInformationFileName + " does not exist")
+	b, err := readFromFile(filepath.Join(homeDir, saveInformationFileName))
+	if err != nil {
+		return &info, err
 	}
-	b, err := ioutil.ReadFile(fpd)
 	if err := json.Unmarshal(b, &info); err != nil {
 		return &info, err
 	}
 	return &info, err
 }
 
+// removeFile delete specify file
+func removeFile(filePath string) error {
+	if err := os.Remove(filePath); err != nil {
+		return err
+	}
+	return nil
+}
+
 func removeInformation() {
 	fpd := filepath.Join(homeDir, saveInformationFileName)
-	if err := os.Remove(fpd); err != nil {
-		log.Fatalln("delete "+fpd, " failed")
+	if err := removeFile(fpd); err != nil {
+		log.Fatal("delete file " + fpd + " failed")
 	}
 }
 
+// writeToFile save SomeThing to SomeFile
+func writeToFile(filePath string, content []byte) error {
+	f, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	if _, err := f.Write(content); err != nil {
+		return err
+	}
+	if err := f.Sync(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// saveInformation save information that cantain appid and secret to ~/.simple-dict
 func saveInformation(info *Information) error {
 	fp := filepath.Join(homeDir, saveInformationFileName)
 
@@ -55,16 +96,7 @@ func saveInformation(info *Information) error {
 	if err != nil {
 		return err
 	}
-	f, err := os.OpenFile(fp, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	if _, err := f.Write(j); err != nil {
-		return err
-	}
-	if err := f.Sync(); err != nil {
+	if err := writeToFile(fp, j); err != nil {
 		return err
 	}
 	return nil
